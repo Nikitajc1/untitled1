9821acab94e88e597e2f76f69ae5504436870ff3
Nikita U <nikitajc@mail.ru>
2022-11-03 19:43:14 +0300

    TaxSystem2

--- src/Main2.java
+++ src/Main2.java
@@ -1,2 +1,13 @@
-package PACKAGE_NAME;public class Main2 {
+public class Main2 {
+    public static void main(String[] aargs) {
+        int income = 7800;
+        int spending = 100000;
+
+        int tax = income * 6 / 100;
+        int tax2 = (income - spending) * 15 / 100;
+
+        System.out.println("Ваш налог на первой системе: " + tax + " рублей");
+        System.out.println("Ваш налог на второй системе: " + tax2 + " рублей");
+
+    }
 }
